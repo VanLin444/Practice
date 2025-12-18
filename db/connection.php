127633e4a9ca4807ec516ef2beee67c1ea4d32f6
VanLin <onel1n.van@yandex.ru>
2025-12-18 23:37:00 +0300

    test connection

--- db/connection.php
+++ db/connection.php
@@ -1,2 +1,18 @@
 <?php
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=films;charset=utf8mb4',
+    'root',
+    'root',
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]
+);
+
+$stmt = $pdo->query("SELECT Title FROM films");
+$users = $stmt->fetchAll();
+echo '<pre>';
+print_r($users);
+echo '</pre>';
+
 
